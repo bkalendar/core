5f6ddde4bb599ce9968a35e06cf03c805f077fd9
iceghost <khangemail22@gmail.com>
2022-04-11 12:32:24 +0700

    Add documentations to timetable parser

--- src/timetable.ts
+++ src/timetable.ts
@@ -1,18 +1,28 @@
 import type { SemesterContext } from "./date_utils";
 import type { EntryRaw, Entry } from "./entry";
 import { parseEntry } from "./entry";
-import { resolveTimetables } from './resolver';
 
+/**
+ * A raw timetable which knows nothing about machine dates.
+ */
 export interface TimetableRaw extends SemesterContext {
     entries: EntryRaw[];
 }
 
+/**
+ * A resolved timetable with resolved dates and weeks.
+ */
 export interface Timetable extends SemesterContext {
     entries: Entry[];
     /** 00:00 UTC Monday of the first week */
     start: Date;
 }
 
+/**
+ * Parse the paste result into a list of raw timetables (all semesters)
+ * @param raw what you get from Ctrl-A and copy at
+ * [stinfo](https://mybk.hcmut.edu.vn/stinfo).
+ */
 export function parseTimetables(raw: string): TimetableRaw[] {
     const pattern =
         /Học kỳ (?<semester>\d) Năm học (?<yearFrom>\d+) - (?<yearTo>\d+)\n[^\n]*\n[^\n]*\n(?<entries>(?:[^](?!Tổng số tín chỉ đăng ký))*)/g;
@@ -25,8 +35,4 @@ export function parseTimetables(raw: string): TimetableRaw[] {
             .map((rawEntry) => parseEntry(rawEntry.trim()))
             .filter(Boolean),
     }));
-}
-
-export function parseAndResolveTimetables(raw: string): Timetable[] {
-    return resolveTimetables(parseTimetables(raw));
 }
