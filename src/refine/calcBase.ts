7c246df742412f6b72b52489599c461d04e607b8
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2022-08-16 12:17:10 +0700

    fix: semester 2 edge case

--- src/refine/calcBase.ts
+++ src/refine/calcBase.ts
@@ -1,27 +1,35 @@
 import { addWeeks, startOfWeek, subWeeks } from "date-fns";
 import type { Semester, Weeks } from "../parser/index.js";
 
-export function calcBase(weeks: Weeks, semester: Semester): Date {
+export function calcBase(weeks: Weeks, { semester, year }: Semester): Date {
 	// case 1: has a new year break, hence it span 2 years
 	if (weeks.newYear !== null) {
-		// is there a better way to handle timezone??? *wink wink Temporal*
-		const newYear = new Date(semester.year + 1, 0, 4);
+		const newYear = new Date(year + 1, 0, 4);
 		// offset new year into base date
 		const base = startOfWeek(subWeeks(newYear, weeks.newYear), { weekStartsOn: 1 });
 		return base;
 	}
 
 	// case 2: no new year break, semester 1 always starts early
-	else if (semester.semester == 1) {
-		const oldYear = new Date(semester.year, 0, 4);
-		const base = startOfWeek(addWeeks(oldYear, weeks.base! - 1), { weekStartsOn: 1 });
-		return base;
+	else if (semester == 1) {
+		return baseStarts("early");
 	}
 
-	// case 3: no new year break, semester 2 and 3 always starts late
-	else {
-		const newYear = new Date(semester.year + 1, 0, 4);
-		const base = startOfWeek(addWeeks(newYear, weeks.base! - 1), { weekStartsOn: 1 });
+	// case 3: no new year break, semester 2 MAY start early
+	else if (semester == 2) {
+		return baseStarts(weeks.base! >= 26 ? "early" : "late");
+	}
+
+	// case 4: no new year break, semester 3 always starts late
+	else if (semester == 3) {
+		return baseStarts("late");
+	}
+
+	throw new Error(`unhandled semester: ${semester}`);
+
+	function baseStarts(type: "early" | "late") {
+		let week1 = new Date(type == "early" ? year : year + 1, 0, 4);
+		const base = startOfWeek(addWeeks(week1, weeks.base! - 1), { weekStartsOn: 1 });
 		return base;
 	}
 }
